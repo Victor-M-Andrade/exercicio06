da23c24ddca5e1b96120269d06e94d5fa97d229c
Victor Andrade <victorma2000@live.com>
2021-03-15 20:52:52 -0300

    Alterando apos a correção do professor

--- src/main/java/exercicio06/Main.java
+++ src/main/java/exercicio06/Main.java
@@ -3,31 +3,48 @@ package exercicio06;
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		Main app = new Main();
 		app.start();
 
 	}
-	
+
 	private void start() {
+
+		String[] carros = { "civic", "gol", "palio", "uno" };
+
+		for (int i = 0; i < carros.length; i++) {
+
+			String carro = carros[i];
+			System.out.println("Nome carro: " + carro);
+		}
+
+		System.out.println("--------------------------");
+
+		carros[1] = "L200";
+
+		for (String carro : carros) {
+
+			System.out.println("Nome carro: " + carro);
+		}
+
+		System.out.println("--------------------------");
+
+		int tamanhoDoNovoArray = carros.length + 1;
+		String[] novoArrayDeCarros = new String[tamanhoDoNovoArray];
+
+		for (int i = 0; i < carros.length; i++) {
+
+			String carro = carros[i];
+			novoArrayDeCarros[i] = carro;
+		}
 		
-		String[] array = new String[5];
+		novoArrayDeCarros[tamanhoDoNovoArray - 1] = "Gol";
 		
-		array [0] = " Civic ";
-		array [1] = " Gol ";
-		array [2] = " Palio ";
-		array [3] = " Uno ";
-		array [4] = "  ";
-		
-		System.out.println("Nome do carro : " + array[0] + array[1] + array[2] + array[3]);
-		
-		for (int i = 0; i< array.length; i++);
+		for (String carro : novoArrayDeCarros) {
+			System.out.println("Nome carro: " + carro);
 			
-			array[1] = "L200";
-			System.out.println("Nome do carro : " + array[0] + array[1] + array[2] + array[3]);
-			array[4] = "Gol";
-			System.out.println("Nome do carro : " + array[0] + array[1] + array[2] + array[3] + array[4]);
-		
+		}
 	}
 
 }
